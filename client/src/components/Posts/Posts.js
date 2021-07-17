d8d22737d03c1d53421e728f25848a385a493add
Mohammed Sayeed <sayeedofficial6220@gmail.com>
2021-07-17 12:04:10 +0530

    Final Changes Made

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,17 +1,24 @@
-import React, { Component } from "react";
-import { useSelector } from "react-redux";
-import Post from "./Post/Post";
-import useStyles from "./styles";
-const Posts = () => {
-  const classes = useStyles();
+import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+
+import Post from './Post/Post';
+import useStyles from './styles';
+
+const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
-  console.log(posts);
+  const classes = useStyles();
+
   return (
-    <div>
-      POSTS
-      <Post />
-      <Post />
-    </div>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
+    )
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -1,41 +1,24 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import postRoutes from "./router/posts.js";
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
-app.use(
-  bodyParser.json({
-    limit: "30mb",
-    extended: true,
-  })
-);
-
-app.use(
-  bodyParser.urlencoded({
-    limit: "30mb",
-    extended: true,
-  })
-);
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-const connectionUrl =
-  "mongodb+srv://sayeed:test123@memories.qfvur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+app.use('/posts', postRoutes);
 
-mongoose
-  .connect(connectionUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
-  )
-  .catch(() => console.log(error.message));
+const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const PORT = process.env.PORT|| 5000;
 
-mongoose.set("useFindAndModify", false);
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
+mongoose.set('useFindAndModify', false);
